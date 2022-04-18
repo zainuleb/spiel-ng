0a8e69504efa47bf2a2d58787858658a93483594
Zain Ul Ebad <zainulebadd@gmail.com>
2022-04-18 06:30:26 -0700

    pipes-practice-done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'spiel';
   imgURL = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+  currentDate = new Date();
+  cost = '2020'
 
   changeImage(e: KeyboardEvent) {
     this.imgURL = (e.target as HTMLInputElement).value;
   }
+
   getName() {
     return this.title;
+  }
+
+  logImg(event: string) {
+    console.log(event)
   }
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+
+export class PostsComponent implements OnInit {
+
   @Input() postImg = "";
+  @Output() imageSelected = new EventEmitter<string>();
 
   constructor() {
-
-    console.log(this.postImg)
-
+    console.log('Cons', this.postImg)
   }
+
+  ngOnInit(): void {
+    console.log('Init', this.postImg)
+  }
+
 }
