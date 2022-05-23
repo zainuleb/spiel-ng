f4ecda09806d0641f9967504191cd425d1f03022
Zain Ul Ebad <zainulebadd@gmail.com>
2022-05-23 08:10:24 -0700

    Saving progress

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TabComponent implements OnInit {
 
   @Input() tabTitle = ''
+  @Input() active = false;
 
   constructor() { }
 
