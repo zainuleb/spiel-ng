6f527d400de58e302748e7d9020f7c88adc3fda5
zainuleb <p166024@nu.edu.pk>
2022-06-13 04:25:39 +0500

    Adding ngx-mask for validating phone numbers

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,23 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
-
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
+  @Input() control: FormControl = new FormControl();
+  @Input() label: string = '';
+  @Input() type: string = 'text';
 
-  @Input() control: FormControl = new FormControl()
-  @Input() label: string = ''
-  @Input() type: string = 'text'
+  constructor() {}
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -4,15 +4,27 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
   email = new FormControl('', [Validators.required, Validators.email]);
-  age = new FormControl('', [Validators.required, Validators.min(12), Validators.max(99)]);
-  password = new FormControl('', [Validators.required, Validators.min(12)]);
-  confirm_password = new FormControl('', [Validators.required, Validators.min(12)]);
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(12),
+    Validators.max(99),
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.min(12),
+    Validators.pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/gm
+    ),
+  ]);
+  confirm_password = new FormControl('', [
+    Validators.required,
+    Validators.min(12),
+  ]);
   phone_number = new FormControl('', [Validators.required, Validators.min(10)]);
 
   registerForm = new FormGroup({
@@ -21,13 +33,10 @@ export class RegisterComponent {
     age: this.age,
     password: this.password,
     confirm_password: this.confirm_password,
-    phone_number: this.phone_number
+    phone_number: this.phone_number,
   });
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
