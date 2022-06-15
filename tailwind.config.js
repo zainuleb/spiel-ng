5e7928b1c97cffdb09db2bb372ef80cc7fedc894
zainuleb <p166024@nu.edu.pk>
2022-06-15 05:58:47 +0500

    disabled submit button usin g form-invalid, configured tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      opacity: ["disabled"],
+      backgroundColor: ["disabled"],
+    },
   },
   plugins: [],
 };
