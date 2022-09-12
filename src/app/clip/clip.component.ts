616f35228e8d79c06ec3ec8f9fca8842fbdd1ae7
zainuleb <zainulebadd@gmail.com>
2022-09-12 10:07:15 +0500

    query params on manage section

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
-  styleUrls: ['./clip.component.css']
+  styleUrls: ['./clip.component.css'],
 })
 export class ClipComponent implements OnInit {
+  id = '';
 
-  constructor() { }
+  constructor(public route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params.id;
+    });
   }
-
 }
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css'],
 })
 export class ManageComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  videoOrder = '1';
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.route.queryParamMap.subscribe((params: Params) => {
+      this.videoOrder = params.sort === '2' ? params.sort : '1';
+    });
+  }
 
   ngOnInit(): void {
     this.route.data.subscribe(console.log);
+  }
+
+  sort(event: Event) {
+    const { value } = event.target as HTMLSelectElement;
+    this.router.navigateByUrl(`/manage?sort=${value}`);
   }
 }
