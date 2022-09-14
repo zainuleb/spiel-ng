e50f7a17d585514b94420e5ad5153242de5a5462
zainuleb <zainulebadd@gmail.com>
2022-09-14 11:36:35 +0500

    adding title to upload form

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
@@ -9,6 +9,12 @@ export class UploadComponent implements OnInit {
   isDragover = false;
   file: File | null = null;
   nextStep = false;
+
+  title = new FormControl('', [Validators.required, Validators.minLength(3)]);
+
+  uploadForm = new FormGroup({
+    title: this.title,
+  });
 
   constructor() {}
 
@@ -20,7 +26,15 @@ export class UploadComponent implements OnInit {
     this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
 
     if (!this.file || this.file.type !== 'video/mp4') {
+      return;
     }
+
+    this.title.setValue(this.file?.name.replace(/\.[^/.]+$/, ''));
+
     this.nextStep = true;
+  }
+
+  uploadFile() {
+    console.log('File Uploaded');
   }
 }
