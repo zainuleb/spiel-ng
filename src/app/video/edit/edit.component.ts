3228b7d0598edf1e32a0f6ab991692d4e8993316
zainuleb <zainulebadd@gmail.com>
2022-09-16 05:30:39 +0500

    binding the update form

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,19 +1,43 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import IClip from 'src/app/modals/clip.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css'],
 })
-export class EditComponent implements OnInit, OnDestroy {
+export class EditComponent implements OnInit, OnChanges, OnDestroy {
   @Input() activeClip: IClip | null = null;
+
+  clipID = new FormControl('');
+  title = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  editForm = new FormGroup({
+    title: this.title,
+    id: this.clipID,
+  });
 
   constructor(private modal: ModalService) {}
 
   ngOnInit(): void {
     this.modal.register('editClip');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!this.activeClip) {
+      return;
+    }
+
+    this.clipID.setValue(this.activeClip.docID);
+    this.title.setValue(this.activeClip.title);
   }
 
   ngOnDestroy(): void {
