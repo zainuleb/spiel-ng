e15925a9caef83e2cb914dd7e881c56c3ce24166
zainuleb <zainulebadd@gmail.com>
2022-09-26 03:24:35 +0500

    bottom of page height scroll foo

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
-  styleUrls: ['./clips-list.component.css']
+  styleUrls: ['./clips-list.component.css'],
 })
-export class ClipsListComponent implements OnInit {
-
-  constructor() { }
+export class ClipsListComponent implements OnInit, OnDestroy {
+  constructor() {}
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll);
   }
 
+  handleScroll = () => {
+    const { scrollTop, offsetHeight } = document.documentElement;
+    const { innerHeight } = window;
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+    if (bottomOfWindow) {
+      console.log(bottomOfWindow);
+    }
+  };
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 }
