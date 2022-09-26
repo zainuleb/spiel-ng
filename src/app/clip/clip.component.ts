186b58befa4091f3ecc252011f195649f73e3155
zainuleb <zainulebadd@gmail.com>
2022-09-26 22:09:00 +0500

    view-encapsulation

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import videojs from 'video.js';
+
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrls: ['./clip.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ClipComponent implements OnInit {
   id = '';
+  @ViewChild('videoPlayer', { static: true }) target?: ElementRef;
+  player?: videojs.Player;
 
   constructor(public route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.player = videojs(this.target?.nativeElement);
+
     this.route.params.subscribe((params: Params) => {
       this.id = params.id;
     });
