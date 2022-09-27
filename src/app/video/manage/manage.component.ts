2f98c34c0138198d674164c8361a79d290160a06
zainuleb <zainulebadd@gmail.com>
2022-09-28 00:10:59 +0500

    clip link copy to clipboard

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -71,4 +71,14 @@ export class ManageComponent implements OnInit {
       if (element.docID === clip.docID) this.clips.splice(index, 1);
     });
   }
+
+  async copyToClipBoard($event: MouseEvent, docID: string | undefined) {
+    $event.preventDefault();
+
+    if (!docID) return;
+    const url = `${location.origin}/clip/${docID}`;
+    await navigator.clipboard.writeText(url);
+
+    alert('Link Copied');
+  }
 }
